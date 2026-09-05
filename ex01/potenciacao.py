e53cdf442ca4876451034e0358dce3c3f4b77d26
Bruno Cunha <brdacunha1@gmail.com>
2026-09-05 19:49:32 -0300

    potenciacao.py
    Criação do exercício de potenciação

---
+++ ex01/potenciacao.py
@@ -0,0 +1,9 @@
+print("Vamos calcular o valor de x=(a+b)²")
+
+# variaveis
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+# Cálculo do x
+x = (a+b)**2
+# Impressão do valor.
+print(f"O valor de x é: {x}")

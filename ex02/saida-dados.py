93bb59233303409ca48357bf1c1a092fb15e34a0
Bruno Cunha <brdacunha1@gmail.com>
2026-09-05 20:28:10 -0300

    saida-dados
    Exercício de  saída de dados

---
+++ ex02/saida-dados.py
@@ -0,0 +1,12 @@
+produto1 = "computador"
+produto2 = "TV"
+preco1 = 2100.5
+preco2 = 1830.0
+idade = 30
+genero = "F"
+codigo = 5291
+
+print(f"O produto {produto1} custa {preco1:.2f}")
+print(f"O produto {produto2} custa {preco2:.2f}")
+print(f"O código de venda é {codigo}")
+print(f"Dados do cliente: gênero = {genero} idade = {idade}")

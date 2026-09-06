5830efb3a7727eec29ab80f2ab1762652726e22f
Bruno Cunha <brdacunha1@gmail.com>
2026-09-05 23:07:15 -0300

    area-trapezio

---
+++ ex03/area-trapezio.py
@@ -0,0 +1,9 @@
+print("Vamos calcular a área de um trapézio!")
+
+# Declaração de variaveis
+baseMaior = float(input("Digite o valor da base maior: "))
+baseMenor = float(input("Digite o valor da base menor: "))
+altura = float(input("Digite o valor da altura: "))
+area = (baseMaior+baseMenor)/2*altura
+
+print(f"A área do trapézio informado é de: {area}")

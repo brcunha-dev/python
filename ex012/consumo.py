cb39ce98c9de9aeffb1929157657c631246a13a8
Bruno Cunha <brdacunha1@gmail.com>
2026-09-07 20:25:25 -0300

    Consumo médio de combústivel

---
+++ ex012/consumo.py
@@ -0,0 +1,9 @@
+print("Consumo médio de combústivel")
+
+# variáveis
+distancia = float(input("Distancia percorrida: "))
+combustivelGasto = float(input("Combústivel gasto: "))
+
+consumoMedio = distancia/combustivelGasto
+
+print(f"Consumo médio: {consumoMedio:.2f}")

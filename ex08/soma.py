330ad79587d201419117e5232a065ab3876f7d9a
Bruno Cunha <brdacunha1@gmail.com>
2026-09-07 19:12:24 -0300

    Soma de dois valores

---
+++ ex08/soma.py
@@ -0,0 +1,10 @@
+print("Somando dois valores!")
+
+# variáveis
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+
+# Soma
+soma = x+y
+
+print(f"SOMA: {soma}")
